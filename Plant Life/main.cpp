72a01647b94eb8a0c0e7b3530b80943e720fd717
Eric <eric.elmoznino@gmail.com>
2017-02-15 01:54:08 -0500

    Initial Commit

---
+++ Plant Life/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Plant Life
+//
+//  Created by Eric Elmoznino on 2017-02-15.
+//  Copyright © 2017 Eric Elmoznino. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
